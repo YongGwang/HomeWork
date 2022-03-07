6bad40e87b90482e2b39033b8cac2e89f9ed0b33
DESKTOP-IEVA8VB\SoSoSon <ellinasong@gmail.com>
2022-03-07 22:56:35 +0900

    コメント追加、Detail、機能追加

--- server.js
+++ server.js
@@ -1,48 +1,66 @@
 const { response } = require('express');
 const express = require('express');
 const app = express();
+//port-------------------------------------
+const port = 8080
+//-----------------------------------------
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 require('dotenv').config();
-
 //middleWareを使う
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(methodOverride('_method'));
 //staticファイルを使うためにpublicフォルダーを指定
 app.use('/public', express.static('public'));
-
 app.set('view engine', 'ejs');
 
+//mongoDB---------------------------------------------------------------------------------------------------------------------------------------------------
 const MongoClient = require('mongodb').MongoClient;
 MongoClient.connect("mongodb+srv://thdwo123:dyd123@cluster0.xylcy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", function(error, client){
     if(error) return console.log(error)
     db = client.db('ToDo_App');
 
     //environment variableの情報によってサーバーを開けてください。
-    app.listen(8080, function() {
-        console.log('listening on 8080');
+    app.listen(port, function() {
+        console.log('listening at http://localhost:%s', port)
     });
 
-    app.post('/register', function(req, response){
-        var tempIsAdmin = false;
-        if(req.body.isadmin =="on") tempIsAdmin = true;
+    //アカウントをデーターベースに登録
+    app.post('/register', nocache, function(req, response){
 
-        db.collection('login').insertOne( { id: req.body.id, pw: req.body.pw }, function(error, result){
+        var textID = req.body.id
+        var textPW = req.body.pw
 
-            //console.log(req.body.isadmin);
-            response.redirect('/')
-        })
-        //Need to develop ID duplicate verification function.
+        //ID,PW文字数制限
+        if(textID.length < 6 || textID.length > 16){
+            response.status(400).send('IDの文字数が6未満か16を超過しています。')
+        }else if(textPW.length < 6 || textPW.length > 16) {
+            response.status(400).send('PWの文字数が6未満か16を超過しています。')
+        }else {
+            db.collection('login').findOne({id:req.body.id},function(error,result){
+                //同じIDがあるか判定
+                if(result != null){ 
+                    response.status(400).send('同じIDが存在しています。')
+                } else{
+                    console.log('生成可能なIDです。')
+                    var tempIsAdmin = false;
+                    if(req.body.isadmin =="on") tempIsAdmin = true;
+                    db.collection('login').insertOne( { id: req.body.id, pw: req.body.pw, admin: tempIsAdmin }, function(error, result){
+                        response.redirect('/')
+                    })
+                }
+            })
+        }
     })
 
-    app.post('/add',  function(req, response){
+    //post追加
+    app.post('/add', nocache, IsPostBlank, function(req, response){
         //response.send('send Over');
         db.collection('counter').findOne({name: 'postNumber'}, function(error, result){
             if(error) return console.log(error)
             //console.log(result.totalPost);
             var totalPostNum = parseInt(result.totalPost);
             var productStock = parseInt(req.body.Stock);
-            //
             var post = { _id: totalPostNum + 1, writer: req.user.id, writerCode: req.user._id, ProductName: req.body.ProductName, ProductInfo: req.body.Info,  Stock: productStock }
 
             db.collection('post').insertOne( post, function(error, result){
@@ -56,10 +74,10 @@ MongoClient.connect("mongodb+srv://thdwo123:dyd123@cluster0.xylcy.mongodb.net/my
 
         response.redirect('/list')
     });
-    
 });
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------
 
-//LOG IN----------------------------------------------------------------------------
+//LOG IN-------------------------------------------------------------------------------------------------------------------------------------------
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const session = require('express-session');
@@ -68,14 +86,13 @@ app.use(session({secret: 'password', resave: true, saveUninitialized: false}));
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.get('/logout', function(req, response) {
-
-    console.log(req.user.id + '　さんがLogOutしました。')
+app.get('/logout', nocache, function(req, response) {
+    console.log(req.user.id + ' さんがLogOutしました。')
     response.clearCookie('connect.sid');
     response.redirect('/');
 });
 
-app.get('/login', function(req, response){
+app.get('/login', nocache, function(req, response){
     response.render('login.ejs', {user: req.user})
 });
 
@@ -83,35 +100,11 @@ app.post('/login', passport.authenticate('local', {
     failureRedirect: '/Loginfail'
 }), function(req, response){
     console.log(req.user);
-    
+
     response.redirect('/list')
 });
 
-app.get('/mypage', function(req, response){
-    response.render('mypage.ejs', {user: req.user});
-});
-
-//Confirm Login: middleWare
-function AYLogin(req, response, next){
-    if(req.user){
-        next()
-    } else {
-        response.send('You are not logged in. please getting back!')
-    }
-}
-
-//Confirm Writer: middleWare
-function AYWriter(req, response, next){
-    req.body._id = parseInt(req.body._id);
-    console.log(req.body._id)
-    console.log(req.user.id)
-    if(req.user.id == req.body._id){
-        next()
-    } else {
-        response.send('You are not this post\'s writer!! please getting back!!')
-    }
-}
-
+//session
 passport.use(new LocalStrategy({
     usernameField: 'id',
     passwordField: 'pw',
@@ -137,7 +130,6 @@ passport.use(new LocalStrategy({
 passport.serializeUser(function(user, done){
     done(null, user.id)
 });
-
 passport.deserializeUser(function(userID, done){
     //ここでユーザー情報を使ってmypageを構成する。
     db.collection('login').findOne({id : userID}, function(error, result){
@@ -145,23 +137,27 @@ passport.deserializeUser(function(userID, done){
         done(null, result)
     })
 });
+//--------------------------------------------------------------------------------------------------------------------------------------------------
 
-
-//----------------------------------------------------------------------------------
-
-app.get('/', function(req, response){
+//Homeへ移動----------------------------------------------------------------------
+app.get('/', nocache, function(req, response){
     db.collection('post').find().toArray(function(error, result){
         if(error) return console.log(error)
         //console.log(result);
         response.render('boxTest.ejs', {posts: result, user: req.user})
     });
 });
+//--------------------------------------------------------------------------------
 
-app.get('/write', AYLogin, function(req, response){
+//ポスト作成ページ移動--------------------------------------------------------------
+app.get('/write', nocache, AYLogin, function(req, response){
+
     response.render('write.ejs', {user: req.user})
 });
+//--------------------------------------------------------------------------------
 
-app.get('/list',AYLogin , function(req, response){
+//リストアップページへ移動----------------------------------------------------------
+app.get('/list', nocache, AYLogin, function(req, response){
 
     db.collection('post').find().toArray(function(error, result){
         if(error) return console.log(error)
@@ -169,8 +165,10 @@ app.get('/list',AYLogin , function(req, response){
         response.render('list.ejs', {posts : result, user: req.user})
     });
 });
+//--------------------------------------------------------------------------------
 
-app.get('/search', (req, response) => {
+//検索----------------------------------------------------------------------------
+app.get('/search', nocache, (req, response) => {
     console.log(req.query)
 
     var searchRequirement = [
@@ -183,30 +181,115 @@ app.get('/search', (req, response) => {
             }
           }
         }
-    ] 
+    ]
 
     db.collection('post').aggregate(searchRequirement).toArray((error, result) => {
         console.log(result)
         response.render('search.ejs', {posts : result, user : req.user})
     })
 });
+//----------------------------------------------------------------------------------
 
-app.delete('/delete', function(req, response){
+//削除------------------------------------------------------------------------------
+app.delete('/delete', nocache, AYWriter, function(req, response){
     console.log('Request to delete');
-    console.log(req.body._id);
+    // console.log(req.body.postData._id);
 
-    //postNumber
-    req.body._id = parseInt(req.body._id);
-    var toData = { _id : req.body._id, writerCode : req.user._id}
+    req.body.postData._id = parseInt(req.body.postData._id)
+    var toData = {}
+
+    if(req.user.admin){
+        toData = { _id : req.body.postData._id }
+    } else { 
+        toData = { _id : req.body.postData._id, writerCode : req.user._id }
+    }
 
     db.collection('post').deleteOne(toData, function(error, result){
         if(error) { return console.log(error) }
         console.log('delete is Over')
         if(result) {console.log(result)}
-        response.status(200).send({ message : '削除に成功しました。' })
+        response.status(200).send({ message: '削除成功' });
+    })
+});
+//-----------------------------------------------------------------------------------
+
+
+//修正------------------------------------------------------------------------------
+app.put('/edit', nocache, IsPostBlank, function(req, response){
+    db.collection('post').updateOne({ _id: parseInt(req.body.id) }, { $set : { ProductName: req.body.ProductName, Info: req.body.ProductInfo, Stock: req.body.Stock }}, function(error, result){
+        if(error) return console.log(error)
+        console.log('修正完了')
+        //response.status(200).send('修正成功');
+        response.redirect('/list')
     })
 });
 
+app.get('/edit/:id', nocache, function(req, response){
+    console.log('Request to Edit');
+
+    db.collection('post').findOne({_id : parseInt(req.params.id)}, function(error, result){
+        if(error) { return console.log(error) }
+
+        console.log(req.params)
+
+        response.render('edit.ejs', {post : result} );
+    })
+});
+//----------------------------------------------------------------------------------
+
+
+//MiddleWare------------------------------------------------------------------------
+//Confirm Login: middleWare
+function AYLogin(req, response, next){
+    if(req.user){
+        next()
+    } else {
+        response.send('You are not logged in. please getting back!')
+    }
+}
+
+//Confirm Writer: middleWare
+function AYWriter(req, response, next){
+    var tempUserCode = req.user._id.toString()
+    var tempPostCode = req.body.postData.wcode.toString()
+    //console.log(tempUserCode)
+    //console.log(tempPostCode)
+
+    if(tempUserCode == tempPostCode || req.user.admin == true){
+        next()
+    } else {
+        response.status(400).send('あなたが作成した文書ではありません。');
+    }
+};
+
+//Confirm Post Input Blank
+function IsPostBlank(req, response, next){
+
+    console.log(req.body)
+    var textPName = req.body.ProductName
+    var textPInfo = req.body.Info
+    var PStock = parseInt(req.body.Stock)
+
+    if(textPName === '' || textPInfo === '' || isNaN(PStock)){
+        response.status(400).send('ポストに空欄があります。');
+    }else {
+        next()
+    }
+}
+//----------------------------------------------------------------------------------
+
+//back制限--------------------------------------------------------------------------
+function nocache(req, response, next) {
+    response.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+    response.header('Expires', '-1');
+    response.header('Pragma', 'no-cache');
+    next();
+}
+//----------------------------------------------------------------------------------
+
+
+//※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※
+//未実装----------------------------------------------------------------------------
 //detail/ページをget要請をしたら
 app.get('/detail/:id', function(req, response){
     db.collection('post').findOne({ _id : parseInt(req.params.id) }, function(error, result){
@@ -216,30 +299,13 @@ app.get('/detail/:id', function(req, response){
     })
 });
 
-//Edit----------------------------------------------------------------------------
-app.put('/edit', function(req, response){
-    db.collection('post').updateOne({ _id: parseInt(req.body.id) }, { $set : { ProductName: req.body.ProductName, Info: req.body.ProductInfo, Stock: req.body.Stock }}, function(error, result){
-        if(error) return console.log(error)
-        console.log('修正完了')
-        response.redirect('/list')
-    })
+app.get('/mypage', function(req, response){
+    response.render('mypage.ejs', {user: req.user});
 });
 
-app.get('/edit/:id', function(req, response){
-    console.log('Request to Edit');
-    db.collection('post').findOne({_id : parseInt(req.params.id)}, function(error, result){
-        if(error) {
-            //response.render('boxTest.ejs');
-            return console.log(error)
-        }
-        response.render('edit.ejs', {post : result} );
-    })
-});
-//----------------------------------------------------------------------------------
-
-
-//IMAGE----------------------------------------------------------------------------
+//IMAGE
 let multer = require('multer');
+const res = require('express/lib/response');
 var storage = multer.diskStorage({
     destination : function(req, file, cb){
         cb(null, './public/image')
@@ -262,13 +328,10 @@ app.post('/upload', upload.single('productPic'), function(req, response){
 app.get('/image/:imageName', function(req, response){
     response.sendFile( __dirname + '/public/image/' + req.params.imageName)
 });
-//----------------------------------------------------------------------------------
-
 
 // app.use('/shop', require('./routes/shop.js'));
 // app.use('/board/sub', require('./routes/board.js'));
-
-app.use('/board/sub', require('./routes/board.js'));
+//app.use('/board/sub', require('./routes/board.js'));
 
 // app.get('/shop/shirts', function(req, response){
 //     response.send('This is shirts page.');
@@ -277,3 +340,5 @@ app.use('/board/sub', require('./routes/board.js'));
 // app.get('/shop/pants', function(req, response){
 //     response.send('This is shirts page.');
 // })
+//----------------------------------------------------------------------------------
+//※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※
